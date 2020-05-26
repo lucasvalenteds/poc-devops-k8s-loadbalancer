ea847fba842a668f506000358eed9f2007ba2dc4
Lucas <lucasvalenteds@gmail.com>
2020-05-25 21:08:04 -0300

    Add testing script

---
+++ test.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+cluster_ip=$(minikube ip)
+cluster_port=$(kubectl get service/server -o jsonpath='{.spec.ports[0].nodePort}')
+
+service_url="http://$cluster_ip:$cluster_port"
+
+npx autocannon --connections 30 --method GET --duration 30 $service_url
